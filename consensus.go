198f8ad9bc23405a2c64ea7492a1ca46c060bfcd
Hanzo Dev <dev@hanzo.ai>
2025-07-27 21:39:05 -0500

    Move consensus tools to consensus package

---
+++ consensus.go
@@ -0,0 +1,9 @@
+// Copyright (C) 2025, Lux Industries Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+// Package genesis provides backwards compatibility by redirecting
+// consensus imports to the new standalone consensus package.
+package genesis
+
+// This file exists for backwards compatibility.
+// Please use github.com/luxfi/consensus directly.
